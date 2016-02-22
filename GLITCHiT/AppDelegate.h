ea7b7de90851aab9feed7480ace576cfe033a7b3
Jeremy J Hartmann <jhartman@sfu.ca>
2016-02-21 23:55:48 -0800

    Initial Commit

---
+++ GLITCHiT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GLITCHiT
+//
+//  Created by Jeremy Hartmann on 2016-02-21.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GLITCHiT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GLITCHiT
+//
+//  Created by Jeremy Hartmann on 2016-02-21.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
