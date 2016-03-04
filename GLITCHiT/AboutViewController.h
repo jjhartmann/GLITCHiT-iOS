7788fd3c0173a220badb11dcbe89fe93c5613f58
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-04 02:45:18 -0800

    Add: AboutViewController class.

---
+++ GLITCHiT/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  GLITCHiT
+//
+//  Created by Jeremy Hartmann on 2016-03-04.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
