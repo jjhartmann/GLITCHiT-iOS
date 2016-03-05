c76d82dc113b72cdc8761f5a4681753e80fb9e64
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-05 00:16:02 -0800

    Add: settings view controller class

---
+++ GLITCHiT/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  GLITCHiT
+//
+//  Created by Jeremy Hartmann on 2016-03-05.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
