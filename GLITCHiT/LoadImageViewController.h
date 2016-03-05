7a6884c92c261bb095c7a63e2a813bdd9714035e
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-05 00:18:16 -0800

    Add: Load image View controller class

---
+++ GLITCHiT/LoadImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoadImageViewController.h
+//  GLITCHiT
+//
+//  Created by Jeremy Hartmann on 2016-03-05.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoadImageViewController : UIViewController
+
+@end
---
+++ GLITCHiT/LoadImageViewController.m
@@ -0,0 +1,37 @@
+//
+//  LoadImageViewController.m
+//  GLITCHiT
+//
+//  Created by Jeremy Hartmann on 2016-03-05.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import "LoadImageViewController.h"
+
+@interface LoadImageViewController ()
+
+@end
+
+@implementation LoadImageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
