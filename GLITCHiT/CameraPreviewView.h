39c7085b5aab0c5e12d99641d0d01185fedc21e1
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-05 22:12:21 -0800

    Add: Custom UIView Class for camera preview.

---
+++ GLITCHiT/CameraPreviewView.h
@@ -0,0 +1,13 @@
+//
+//  CameraPreviewView.h
+//  GLITCHiT
+//
+//  Created by Jeremy Hartmann on 2016-03-05.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraPreviewView : UIView
+
+@end
---
+++ GLITCHiT/CameraPreviewView.m
@@ -0,0 +1,21 @@
+//
+//  CameraPreviewView.m
+//  GLITCHiT
+//
+//  Created by Jeremy Hartmann on 2016-03-05.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import "CameraPreviewView.h"
+
+@implementation CameraPreviewView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
